42e6121c7d78fd9b4d7856f4524355265cd336cf
Ercan Havare <ercanhavare@gmail.com>
2020-07-03 07:05:11 +0300

    hook

--- resources/js/router.js
+++ resources/js/router.js
@@ -1,13 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 Vue.use(Router)
 
 import firstPage from "./components/pages/myFirstVuePage";
 import newRoutePage from "./components/pages/newRoutePage";
-import hooks from "./components/pages/basic/hooks"
-import methods from "./components/pages/basic/methods"
+import hooks from "./components/pages/basic/hooks";
+import methods from "./components/pages/basic/methods";
+
+import home from "./components/pages/home";
 
 const routes = [
+    // project routes ....
+    {
+        path: '/',
+        component: home
+    },
+
+    // basic tutorial routes ...
     {
         path: '/my-new-vue-route',
         component: firstPage
@@ -18,8 +28,8 @@ const routes = [
     },
     //  vue hooks
     {
-      path: "/hooks",
-      component: hooks
+        path: "/hooks",
+        component: hooks
     },
     //  more basic
     {
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>LaraFull Blog</title>
+
+        <link rel="stylesheet" href="/css/all.css">
     </head>
     <body>
     <div id="app">
